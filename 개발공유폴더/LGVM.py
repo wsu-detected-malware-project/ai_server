28e38ac2b9bf0362be07538c8c4fbf75767d1491
YUN <68643530+YUN-DAE-YOUNG@users.noreply.github.com>
2025-04-05 20:18:58 +0900

    Update LGVM.py

--- 개발공유폴더/LGVM.py
+++ 개발공유폴더/LGVM.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from lightgbm import LGBMClassifier  # ← LGBM import
+from lightgbm import LGBMClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.pipeline import Pipeline
@@ -18,7 +18,16 @@ df1 = pd.read_csv('./dataset_test.csv')
 
 dropped_df = df.drop(['Name','Malware'],axis=1)
 
-features = [...]  # 생략 (기존 코드와 동일하게 유지)
+features = ['e_magic','e_cblp','e_cp','e_crlc','e_cparhdr','e_minalloc','e_maxalloc','e_ss','e_sp','e_csum','e_ip','e_cs','e_lfarlc','e_ovno','e_oemid','e_oeminfo',
+            'e_lfanew','Machine','NumberOfSections','TimeDateStamp','PointerToSymbolTable','NumberOfSymbols','SizeOfOptionalHeader','Characteristics',
+            'Magic','MajorLinkerVersion','MinorLinkerVersion','SizeOfCode','SizeOfInitializedData','SizeOfUninitializedData','AddressOfEntryPoint',
+            'BaseOfCode','ImageBase','SectionAlignment','FileAlignment','MajorOperatingSystemVersion','MinorOperatingSystemVersion','MajorImageVersion',
+            'MinorImageVersion','MajorSubsystemVersion','MinorSubsystemVersion','SizeOfHeaders','CheckSum','SizeOfImage','Subsystem','DllCharacteristics',
+            'SizeOfStackReserve','SizeOfStackCommit','SizeOfHeapReserve','SizeOfHeapCommit','LoaderFlags','NumberOfRvaAndSizes','SuspiciousImportFunctions',
+            'SuspiciousNameSection','SectionsLength','SectionMinEntropy','SectionMaxEntropy','SectionMinRawsize','SectionMaxRawsize','SectionMinVirtualsize',
+            'SectionMaxVirtualsize','SectionMaxPhysical','SectionMinPhysical','SectionMaxVirtual','SectionMinVirtual','SectionMaxPointerData','SectionMinPointerData',
+            'SectionMaxChar','SectionMainChar','DirectoryEntryImport','DirectoryEntryImportSize','DirectoryEntryExport','ImageDirectoryEntryExport','ImageDirectoryEntryImport',
+            'ImageDirectoryEntryResource','ImageDirectoryEntryException','ImageDirectoryEntrySecurity']  # 생략 (기존 코드와 동일하게 유지)
 x = dropped_df
 y = df['Malware']
 
